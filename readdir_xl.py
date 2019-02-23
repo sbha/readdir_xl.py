b940542a5f30f3cfbfd9ff9b8e3dce30c4faf9d0
sbha <sbharty@gmail.com>
2019-02-23 08:53:23 -0600

    clean up

--- readdir_xl.py
+++ readdir_xl.py
@@ -1,5 +1,5 @@
 file_type = "test[0-9].xlsx"
-#file_type = "^sample_[0-9]{4}-[0-9]{2}-[0-9]{2}\\.xlsx"
+
 def dir_xl_reader(d, f):
     df_out = pd.DataFrame()
     for file in glob.glob(os.path.join(d, f)):
@@ -12,5 +12,3 @@ def dir_xl_reader(d, f):
 df_xl = dir_xl_reader(dir_path, file_type)
 df_xl
 
-# git remote add origin https://github.com/sbha/readdir_xl.py.git
-# git clone git@github.com:sbha/readdir_xl.py.git
